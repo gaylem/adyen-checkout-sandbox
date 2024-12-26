a0683526ddc889e304232713b998820a48cf64f6
gayle <gayle.martin@adyen.com>
2024-12-26 09:28:31 -0600

    Replace clientKey with env variable

--- src/applepaySessions/applepay.js
+++ src/applepaySessions/applepay.js
@@ -1,31 +1,33 @@
-sessionsDropin().then((session) => {
-  initSession();
-  async function initSession() {
-    const checkout = await AdyenCheckout({
-      clientKey: "test_M35ZRWIW6JHMPOLIAJELF2OYEYIKZQEP",
-      environment: "test",
-      session,
-      onChange: (state, component) => {
-        updateStateContainer(state); // Demo purposes only
-      },
-      onPaymentCompleted: (result, component) => {
-        console.info(result, component);
-        updateResponseContainer(result);
-      },
-      onError: (error, component) => {
-        console.error(error.name, error.message, error.stack, component);
-        updateResponseContainer(response.message);
-      },
-    });
-    const applePayComponent = checkout.create("applepay");
-
-    applePayComponent
-      .isAvailable()
-      .then(() => {
-        applePayComponent.mount("#applepay-container");
-      })
-      .catch((e) => {
-        console.log(e);
+getClientKey().then(clientKey => {
+  sessionsDropin().then((session) => {
+    initSession();
+    async function initSession() {
+      const checkout = await AdyenCheckout({
+        clientKey: clientKey,
+        environment: "test",
+        session,
+        onChange: (state, component) => {
+          updateStateContainer(state); // Demo purposes only
+        },
+        onPaymentCompleted: (result, component) => {
+          console.info(result, component);
+          updateResponseContainer(result);
+        },
+        onError: (error, component) => {
+          console.error(error.name, error.message, error.stack, component);
+          updateResponseContainer(response.message);
+        },
       });
-  }
+      const applePayComponent = checkout.create("applepay");
+
+      applePayComponent
+        .isAvailable()
+        .then(() => {
+          applePayComponent.mount("#applepay-container");
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }
+  });
 });
